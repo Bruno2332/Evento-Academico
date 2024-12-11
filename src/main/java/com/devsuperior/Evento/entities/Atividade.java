b496a459814a08c32e2a7f3c89f415a7fe075044
Bruno Flor de Lys <brunolys23@gmail.com>
2024-12-11 19:22:43 -0300

    Classes Participante e Bloco

--- src/main/java/com/devsuperior/Evento/entities/Atividade.java
+++ src/main/java/com/devsuperior/Evento/entities/Atividade.java
@@ -1,12 +1,21 @@
 package com.devsuperior.Evento.entities;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -26,7 +35,25 @@ public class Atividade {
 	@JoinColumn(name = "categoria_id")
 	private Categoria categoria;
 	
+	@OneToMany(mappedBy = "atividade")
+	private List<Bloco> blocos = new ArrayList<>();
+	
+	@ManyToMany
+	@JoinTable(name = "tb_atividade_participante",
+			joinColumns = @JoinColumn(name = "atividade_id"),
+			inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private Set<Participante> participantes = new HashSet<>();
+	
+	
 	public Atividade() {
+	}
+	
+	public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.categoria = categoria;
 	}
 
 	public Integer getId() {
@@ -60,7 +87,33 @@ public class Atividade {
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-	
-	
 
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	public Set<Participante> getParticipantes() {
+		return participantes;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Atividade other = (Atividade) obj;
+		return Objects.equals(id, other.id);
+	}
 }
